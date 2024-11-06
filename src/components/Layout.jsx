f58d4771441ab78a73349d75d6add23267678f51
arashi-1 <anoopkumar0935@gmail.com>
2024-11-06 19:22:13 +0530

    update

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -12,10 +12,10 @@ const Layout = () => {
         <div>
           <div>
             <div>
-              <h1 className=" font-samarkan text-5xl flex items-center justify-center mt-5">
+              <h1 className="font-samarkan text-5xl text-center mt-5 md:text-6xl lg:text-7xl">
                 Chanakya Neethi
               </h1>
-              <p className="mt-3 text-lg font-semibold text-gray-100 bg-gray-800 p-4 rounded-lg shadow-md ">
+              <p className="mt-3 text-lg font-semibold text-gray-100 bg-gray-800 p-4 rounded-lg shadow-md sm:text-base md:text-lg lg:text-xl">
                 <b className="text-yellow-400">Chanakya Neethi</b> (or Chanakya
                 Niti) is a compilation of aphorisms and teachings by Chanakya,
                 also known as Kautilya or Vishnugupta, who was an ancient Indian
@@ -28,167 +28,24 @@ const Layout = () => {
             </div>
           </div>
         </div>
-        <div className="navigation-buttons flex items-center justify-center gap-96 mb-2">
-          <Link to="/chapter/1">
-            <button className="flex items-center h-16 w-96 rounded-md bg-blue-500 mt-5">
-              <div className="bg-red-600 w-12 h-12 rounded-full ml-2 flex items-center justify-center">
-                <h1 className="text-yellow-500 text-xl font-bold">1</h1>
-              </div>
-              <h1 className="text-2xl ml-24 font-bold">Chapter 1</h1>
-            </button>
-          </Link>
-
-          <Link to="/chapter/2">
-            <button className="flex items-center h-16 w-96 rounded-md bg-blue-500 mt-5">
-              <div className="bg-red-600 w-12 h-12 rounded-full ml-2 flex items-center justify-center">
-                <h1 className="text-yellow-500 text-xl font-bold">2</h1>
-              </div>
-              <h1 className="text-2xl ml-24 font-bold">Chapter 2</h1>
-            </button>
-          </Link>
-        </div>
-        <div className="navigation-buttons flex items-center justify-center gap-96 mb-2">
-          <Link to="/chapter/3">
-            <button className="flex items-center h-16 w-96 rounded-md bg-blue-500 mt-5">
-              <div className="bg-red-600 w-12 h-12 rounded-full ml-2 flex items-center justify-center">
-                <h1 className="text-yellow-500 text-xl font-bold">3</h1>
-              </div>
-              <h1 className="text-2xl ml-24 font-bold">Chapter 3</h1>
-            </button>
-          </Link>
-
-          <Link to="/chapter/1">
-            <button className="flex items-center h-16 w-96 rounded-md bg-blue-500 mt-5">
-              <div className="bg-red-600 w-12 h-12 rounded-full ml-2 flex items-center justify-center">
-                <h1 className="text-yellow-500 text-xl font-bold">4</h1>
-              </div>
-              <h1 className="text-2xl ml-24 font-bold">Chapter 4</h1>
-            </button>
-          </Link>
-        </div>
-        <div className="navigation-buttons flex items-center justify-center gap-96 mb-2">
-          <Link to="/chapter/1">
-            <button className="flex items-center h-16 w-96 rounded-md bg-blue-500 mt-5">
-              <div className="bg-red-600 w-12 h-12 rounded-full ml-2 flex items-center justify-center">
-                <h1 className="text-yellow-500 text-xl font-bold">5</h1>
-              </div>
-              <h1 className="text-2xl ml-24 font-bold">Chapter 5</h1>
-            </button>
-          </Link>
-
-          <Link to="/chapter/1">
-            <button className="flex items-center h-16 w-96 rounded-md bg-blue-500 mt-5">
-              <div className="bg-red-600 w-12 h-12 rounded-full ml-2 flex items-center justify-center">
-                <h1 className="text-yellow-500 text-xl font-bold">6</h1>
-              </div>
-              <h1 className="text-2xl ml-24 font-bold">Chapter 6</h1>
-            </button>
-          </Link>
-        </div>
-        <div className="navigation-buttons flex items-center justify-center gap-96 mb-2">
-          <Link to="/chapter/1">
-            <button className="flex items-center h-16 w-96 rounded-md bg-blue-500 mt-5">
-              <div className="bg-red-600 w-12 h-12 rounded-full ml-2 flex items-center justify-center">
-                <h1 className="text-yellow-500 text-xl font-bold">7</h1>
-              </div>
-              <h1 className="text-2xl ml-24 font-bold">Chapter 7</h1>
-            </button>
-          </Link>
-
-          <Link to="/chapter/1">
-            <button className="flex items-center h-16 w-96 rounded-md bg-blue-500 mt-5">
-              <div className="bg-red-600 w-12 h-12 rounded-full ml-2 flex items-center justify-center">
-                <h1 className="text-yellow-500 text-xl font-bold">8</h1>
-              </div>
-              <h1 className="text-2xl ml-24 font-bold">Chapter 8</h1>
-            </button>
-          </Link>
-        </div>
-        <div className="navigation-buttons flex items-center justify-center gap-96 mb-2">
-          <Link to="/chapter/1">
-            <button className="flex items-center h-16 w-96 rounded-md bg-blue-500 mt-5">
-              <div className="bg-red-600 w-12 h-12 rounded-full ml-2 flex items-center justify-center">
-                <h1 className="text-yellow-500 text-xl font-bold">9</h1>
-              </div>
-              <h1 className="text-2xl ml-24 font-bold">Chapter 9</h1>
-            </button>
-          </Link>
-
-          <Link to="/chapter/1">
-            <button className="flex items-center h-16 w-96 rounded-md bg-blue-500 mt-5">
-              <div className="bg-red-600 w-12 h-12 rounded-full ml-2 flex items-center justify-center">
-                <h1 className="text-yellow-500 text-xl font-bold">10</h1>
-              </div>
-              <h1 className="text-2xl ml-24 font-bold">Chapter 10</h1>
-            </button>
-          </Link>
-        </div>
-        <div className="navigation-buttons flex items-center justify-center gap-96 mb-2">
-          <Link to="/chapter/1">
-            <button className="flex items-center h-16 w-96 rounded-md bg-blue-500 mt-5">
-              <div className="bg-red-600 w-12 h-12 rounded-full ml-2 flex items-center justify-center">
-                <h1 className="text-yellow-500 text-xl font-bold">11</h1>
-              </div>
-              <h1 className="text-2xl ml-24 font-bold">Chapter 11</h1>
-            </button>
-          </Link>
-
-          <Link to="/chapter/1">
-            <button className="flex items-center h-16 w-96 rounded-md bg-blue-500 mt-5">
-              <div className="bg-red-600 w-12 h-12 rounded-full ml-2 flex items-center justify-center">
-                <h1 className="text-yellow-500 text-xl font-bold">12</h1>
-              </div>
-              <h1 className="text-2xl ml-24 font-bold">Chapter 12</h1>
-            </button>
-          </Link>
-        </div>
-        <div className="navigation-buttons flex items-center justify-center gap-96 mb-2">
-          <Link to="/chapter/1">
-            <button className="flex items-center h-16 w-96 rounded-md bg-blue-500 mt-5">
-              <div className="bg-red-600 w-12 h-12 rounded-full ml-2 flex items-center justify-center">
-                <h1 className="text-yellow-500 text-xl font-bold">13</h1>
-              </div>
-              <h1 className="text-2xl ml-24 font-bold">Chapter 13</h1>
-            </button>
-          </Link>
-
-          <Link to="/chapter/1">
-            <button className="flex items-center h-16 w-96 rounded-md bg-blue-500 mt-5">
-              <div className="bg-red-600 w-12 h-12 rounded-full ml-2 flex items-center justify-center">
-                <h1 className="text-yellow-500 text-xl font-bold">14</h1>
-              </div>
-              <h1 className="text-2xl ml-24 font-bold">Chapter 14</h1>
-            </button>
-          </Link>
-        </div>
-        <div className="navigation-buttons flex items-center justify-center gap-96 mb-2">
-          <Link to="/chapter/1">
-            <button className="flex items-center h-16 w-96 rounded-md bg-blue-500 mt-5">
-              <div className="bg-red-600 w-12 h-12 rounded-full ml-2 flex items-center justify-center">
-                <h1 className="text-yellow-500 text-xl font-bold">15</h1>
-              </div>
-              <h1 className="text-2xl ml-24 font-bold">Chapter 15</h1>
-            </button>
-          </Link>
-
-          <Link to="/chapter/1">
-            <button className="flex items-center h-16 w-96 rounded-md bg-blue-500 mt-5">
-              <div className="bg-red-600 w-12 h-12 rounded-full ml-2 flex items-center justify-center">
-                <h1 className="text-yellow-500 text-xl font-bold">16</h1>
-              </div>
-              <h1 className="text-2xl ml-24 font-bold">Chapter 16</h1>
-            </button>
-          </Link>
-        </div>
-        <div>
-          <Link to="/chapter/1">
-            <button className="flex items-center h-16 w-96 rounded-md bg-blue-500 mt-5 ml-14">
-              <div className="bg-red-600 w-12 h-12 rounded-full ml-2 flex items-center justify-center">
-                <h1 className="text-yellow-500 text-xl font-bold">17</h1>
-              </div>
-              <h1 className="text-2xl ml-24 font-bold">Chapter 17</h1>
-            </button>
-          </Link>
+        {/* Responsive Grid */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-8 mx-auto mb-8 max-w-screen-lg px-4">
+          {[...Array(17)].map((_, index) => (
+            <div key={index} className="mb-8">
+              <Link to={`/chapter/${index + 1}`} className="w-full">
+                <button className="flex items-center justify-start h-16 w-full sm:w-96 rounded-md bg-blue-500 mt-4 p-4">
+                  <div className="bg-red-600 w-12 h-12 rounded-full flex items-center justify-center">
+                    <h1 className="text-yellow-500 text-xl font-bold">
+                      {index + 1}
+                    </h1>
+                  </div>
+                  <h1 className="text-2xl ml-6 font-bold text-white">
+                    Chapter {index + 1}
+                  </h1>
+                </button>
+              </Link>
+            </div>
+          ))}
         </div>
         <Outlet /> {/* This will render the content of the nested routes */}
         <Footer />
