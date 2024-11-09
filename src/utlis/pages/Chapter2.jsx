3b3183bb2e31544894692b1ef55237a829f4491e
arashi-1 <anoopkumar0935@gmail.com>
2024-11-09 20:28:34 +0530

    responsive

--- src/utlis/pages/Chapter2.jsx
+++ src/utlis/pages/Chapter2.jsx
@@ -4,8 +4,10 @@ const Chapter2 = () => {
   return (
     <>
       <div className="flex flex-col items-center">
-        <div className="bg-orange-500 h-16 w-full flex items-center justify-center">
-          <h1 className="font-samarkan text-5xl text-black">Chanakya Neethi</h1>
+        <div className="bg-orange-500 h-16 sm:h-20 md:h-24 lg:h-28 w-full flex items-center justify-center">
+          <h1 className="font-samarkan text-3xl sm:text-4xl md:text-5xl lg:text-6xl text-black">
+            Chanakya Neethi
+          </h1>
         </div>
 
         <h1 className="font-samarkan text-5xl mt-12">Chapter 2</h1>
