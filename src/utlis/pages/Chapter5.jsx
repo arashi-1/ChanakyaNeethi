241c7e3a7f55278731e2dd4ddc8744a3a9a3c160
arashi-1 <anoopkumar0935@gmail.com>
2024-11-09 18:37:02 +0530

    responsive

--- src/utlis/pages/Chapter5.jsx
+++ src/utlis/pages/Chapter5.jsx
@@ -3,8 +3,10 @@ import React from "react";
 const Chapter5 = () => {
   return (
     <div className="flex flex-col items-center">
-      <div className="bg-orange-500 h-16 w-full flex items-center justify-center">
-        <h1 className="font-samarkan text-5xl text-black">Chanakya Neethi</h1>
+      <div className="bg-orange-500 h-16 sm:h-20 md:h-24 lg:h-28 w-full flex items-center justify-center">
+        <h1 className="font-samarkan text-3xl sm:text-4xl md:text-5xl lg:text-6xl text-black">
+          Chanakya Neethi
+        </h1>
       </div>
 
       <h1 className="font-samarkan text-5xl mt-12">Chapter 5</h1>
